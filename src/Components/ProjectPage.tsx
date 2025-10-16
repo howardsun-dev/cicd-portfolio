e5111ea49c5ffda1435684700cb9358bd3f232de
Howard Sun <howardsun@pm.me>
2025-10-16 16:05:05 -0400

    cleanup page for projects spa

--- src/Components/ProjectPage.tsx
+++ src/Components/ProjectPage.tsx
@@ -5,6 +5,7 @@ export default function ProjectPage() {
     <div>
       <h1>My Project Page</h1>
       <p>Work in Progress</p>
+      <br></br>
       <Link className="social-link" to="/">
         Home
       </Link>
