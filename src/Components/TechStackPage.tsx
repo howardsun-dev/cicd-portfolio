f518a0c69df1e48163b7c54fd0a4cb8aaf8218f1
Howard Sun <howardsun@pm.me>
2025-11-04 18:30:18 -0500

    temporary tech stack page to be reworked

--- src/Components/TechStackPage.tsx
+++ src/Components/TechStackPage.tsx
@@ -1,19 +1,64 @@
+import React, { useState } from 'react';
 import './ProfilePage.css';
 import { Link } from '@tanstack/react-router';
 
+// add collapsable list for the 3 columns
+
+function CollapsibleListItem({ title, children }) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleExpand = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  return (
+    <div>
+      <div onClick={toggleExpand} style={{ cursor: 'pointer' }}>
+        <h3>
+          {title} {isExpanded ? '▲' : '▼'}
+        </h3>
+      </div>
+      {isExpanded && <div>{children}</div>}
+    </div>
+  );
+}
 export default function TechStackPage() {
   return (
     <div className="profile-container">
       <div className="title">Tech Stack</div>
       <div className="tech-container">
         <div className="column">
-          <li>Javascript</li>
+          <CollapsibleListItem title="Fontend">
+            <ul>
+              <li>React</li>
+              <li>Redux</li>
+              <li>Typescript</li>
+            </ul>
+          </CollapsibleListItem>
         </div>
         <div className="column">
-          <li>Postgres</li>
+          <CollapsibleListItem title="Backend">
+            <ul>
+              <li>Node.js</li>
+              <li>Express</li>
+              <li>Electron.js</li>
+              <li>Python</li>
+              <li>PostgreSQL</li>
+              <li>MongoDB</li>
+              <li>Vite</li>
+              <li>webpack</li>
+            </ul>
+          </CollapsibleListItem>
         </div>
         <div className="column">
-          <li>AWS S3</li>
+          <CollapsibleListItem title="Cloud & Containerization">
+            <ul>
+              <li>AWS</li>
+              <li>Terraform</li>
+              <li>Docker</li>
+              <li>Kubernetes</li>
+            </ul>
+          </CollapsibleListItem>
         </div>
       </div>
       <div style={{ marginTop: '1.5rem' }}>
