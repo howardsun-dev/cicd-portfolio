1ebc22d33352d77907120ff74449add5f551fe0b
Howard Sun <howardsun@pm.me>
2026-04-22 17:56:15 -0400

    removed unused imports

--- src/Components/TechStackPage.tsx
+++ src/Components/TechStackPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './ProfilePage.css';
 import { Link } from '@tanstack/react-router';
 
